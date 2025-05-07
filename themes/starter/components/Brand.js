f3b781694f6e30a81a49ddb0cb51a1dad7f281b0
guangtairan <guangtairan@gmail.com>
2025-05-07 14:49:40 +0800

    Update Brand.js

--- themes/starter/components/Brand.js
+++ themes/starter/components/Brand.js
@@ -13,8 +13,7 @@ export const Brand = () => {
       {/* <!-- ====== Brands Section Start --> */}
       <section className='py-20 dark:bg-dark'>
         <div className='container px-4'>
-          <h2 className='mb-3 text-3xl font-bold text-dark dark:text-white sm:text-4xl md:text-[40px] md:leading-[1.2]'>
-                合作伙伴
+          <h2 className='mb-3 text-3xl font-bold text-dark dark:text-white sm:text-4xl md:text-[40px] md:leading-[1.2]'>合作伙伴
           </h2>
           <div className='-mx-4 flex flex-wrap items-center justify-center gap-8 xl:gap-11'>
             {brands?.map((item, index) => {

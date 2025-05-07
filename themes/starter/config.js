1c37547aeb9a74a8249f80ee9704e982fda89748
guangtairan <guangtairan@gmail.com>
2025-05-07 13:04:17 +0800

    Update config.js

--- themes/starter/config.js
+++ themes/starter/config.js
@@ -22,7 +22,7 @@ const CONFIG = {
 
   // 英雄区配图，如需隐藏，改为空值即可 ''
   STARTER_HERO_PREVIEW_IMAGE: '', // 产品预览图 ，默认读取public目录下图片
-  STARTER_HERO_BANNER_IMAGE: '/images/starter/zgkxydx.png', // hero区下方的全宽图
+  STARTER_HERO_BANNER_IMAGE: '/images/starter/gkdblue.png', // hero区下方的全宽图
 
   // 顶部右侧导航暗流
   // STARTER_NAV_BUTTON_1_TEXT: 'Sign In',
@@ -34,7 +34,7 @@ const CONFIG = {
   // 特性区块
   STARTER_FEATURE_ENABLE: true, // 特性区块开关
   // STARTER_FEATURE_TITLE: '特性', // 特性
-  STARTER_FEATURE_TEXT_1: 'NotionNext的主要特性', // 特性
+  STARTER_FEATURE_TEXT_1: '研究方向', // 特性
   // STARTER_FEATURE_TEXT_2:
   //   'NotionNext的愿景是帮助您简单、无感知地稳定地搭建自己的网站，放大品牌的价值。 ', // 特性
 
@@ -49,7 +49,7 @@ const CONFIG = {
   STARTER_FEATURE_2_BUTTON_URL:
     'https://docs.tangly1024.com/article/notion-next-themes', // 特性2
 
-  STARTER_FEATURE_3_TITLE_1: '地表参数空间尺度转换机理与方法', // 特性3
+  STARTER_FEATURE_3_TITLE_1: '地表参数空间尺度转换', // 特性3
   // STARTER_FEATURE_3_TEXT_1: '基于NextJS开发，更快的响应速度，更好的SEO', // 特性3
   STARTER_FEATURE_3_BUTTON_TEXT: '了解更多', // 特性3
   STARTER_FEATURE_3_BUTTON_URL: 'https://docs.tangly1024.com/article/next-js', // 特性3
@@ -208,7 +208,7 @@ const CONFIG = {
   STARTER_TEAM_ITEMS: [
     {
       STARTER_TEAM_ITEM_AVATAR:
-        'https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2Fa06c61bb-980e-4180-bc18-c15f92c78bb4%2Ftangly1024.jpg?table=collection&id=8e7acf17-de09-4fa1-abde-b5b80ad4a813&t=8e7acf17-de09-4fa1-abde-b5b80ad4a813&width=100&cache=v2',
+        '/images/starter/zhaowei.png',
       STARTER_TEAM_ITEM_NICKNAME: '赵伟',
       STARTER_TEAM_ITEM_DESCRIPTION: '研究员'
     },
@@ -237,27 +237,7 @@ const CONFIG = {
   // STARTER_BLOG_TEXT_2:
   //   '这里会发布一些关于NotionNext的最新动态，包括新的动向、新的未来计划，以及新功能的特性',
 
-  // 联系模块
-  STARTER_CONTACT_ENABLE: true, // 联系模块开关
-  STARTER_CONTACT_TITLE: '联系我们',
-  STARTER_CONTACT_TEXT: '告诉我们您遇到的问题',
-  STARTER_CONTACT_LOCATION_TITLE: '我们的位置',
-  STARTER_CONTACT_LOCATION_TEXT: '中国，福建',
-  STARTER_CONTACT_EMAIL_TITLE: '我们如何帮助您？',
-  STARTER_CONTACT_EMAIL_TEXT: 'mail@tangly1024.com',
-
-  // 嵌入外部表单
-  STARTER_CONTACT_MSG_EXTERNAL_URL: 'https://noteforms.com/forms/yfctc7', // 基于NoteForm创建，将留言数据存在Notion中
-  //   自定义留言表单，以下几个配置暂时废弃
-  //   STARTER_CONTACT_MSG_TITLE: '向我们留言',
-  //   STARTER_CONTACT_MSG_NAME: '姓名',
-  //   STARTER_CONTACT_MSG_EMAIL: '邮箱地址',
-  //   STARTER_CONTACT_MSG_PHONE: '联系电话',
-  //   STARTER_CONTACT_MSG_TEXT: '消息内容',
-  //   STARTER_CONTACT_MSG_SEND: '发送消息',
-  //   STARTER_CONTACT_MSG_THANKS: '感谢您的留言',
-
-  // 合作伙伴的图标
+// 合作伙伴的图标
   STARTER_BRANDS_ENABLE: true, // 合作伙伴开关
   STARTER_BRANDS: [
     {
@@ -293,6 +273,28 @@ const CONFIG = {
   ],
 
   STARTER_FOOTER_SLOGAN: '我们通过技术为品牌和公司创造数字体验。',
+  
+  // 联系模块
+  STARTER_CONTACT_ENABLE: true, // 联系模块开关
+  STARTER_CONTACT_TITLE: '联系我们',
+  STARTER_CONTACT_TEXT: '告诉我们您遇到的问题',
+  STARTER_CONTACT_LOCATION_TITLE: '我们的位置',
+  STARTER_CONTACT_LOCATION_TEXT: '中国，福建',
+  STARTER_CONTACT_EMAIL_TITLE: '我们如何帮助您？',
+  STARTER_CONTACT_EMAIL_TEXT: 'mail@tangly1024.com',
+
+  // 嵌入外部表单
+  STARTER_CONTACT_MSG_EXTERNAL_URL: 'https://noteforms.com/forms/yfctc7', // 基于NoteForm创建，将留言数据存在Notion中
+  //   自定义留言表单，以下几个配置暂时废弃
+  //   STARTER_CONTACT_MSG_TITLE: '向我们留言',
+  //   STARTER_CONTACT_MSG_NAME: '姓名',
+  //   STARTER_CONTACT_MSG_EMAIL: '邮箱地址',
+  //   STARTER_CONTACT_MSG_PHONE: '联系电话',
+  //   STARTER_CONTACT_MSG_TEXT: '消息内容',
+  //   STARTER_CONTACT_MSG_SEND: '发送消息',
+  //   STARTER_CONTACT_MSG_THANKS: '感谢您的留言',
+
+  
 
   // 页脚三列菜单组
   STARTER_FOOTER_LINK_GROUP: [
